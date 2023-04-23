e05815b976d72238acf0f894791d5109bad72c49
YIDA-max <3136271519@qq.com>
2023-04-23 11:28:43 +0800

    添加注释

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,27 @@
-const router = require('koa-router')()
+/*
+ * @Author: YIDA-max 3136271519@qq.com
+ * @Date: 2023-04-23 10:14:56
+ * @LastEditors: YIDA-max 3136271519@qq.com
+ * @LastEditTime: 2023-04-23 10:19:20
+ * @FilePath: /node-koa-mysql/routes/index.js
+ * @Description: 路由主入口
+ */
+const router = require("koa-router")();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-module.exports = router
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,21 @@
-const router = require('koa-router')()
+/*
+ * @Author: YIDA-max 3136271519@qq.com
+ * @Date: 2023-04-23 10:14:56
+ * @LastEditors: YIDA-max 3136271519@qq.com
+ * @LastEditTime: 2023-04-23 11:27:39
+ * @FilePath: /node-koa-mysql/routes/users.js
+ * @Description: 路由入口之一
+ */
+const router = require("koa-router")();
 
-router.prefix('/users')
+router.prefix("/users");
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.get("/", function (ctx, next) {
+  ctx.body = "this is a users response!";
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
-
-module.exports = router
+router.get("/bar", function (ctx, next) {
+  ctx.body = "this is a users/bar response";
+});
+console.log(123);
+module.exports = router;
