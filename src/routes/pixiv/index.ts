f71f0dc2ebf846e88882029361c544a33a36ffc6
YIDA-max <3136271519@qq.com>
2023-05-04 15:05:29 +0800

    获取到对应的id的图片信息的接口

--- src/routes/pixiv/index.ts
+++ src/routes/pixiv/index.ts
@@ -2,7 +2,7 @@
  * @Author: YIDA-max 3136271519@qq.com
  * @Date: 2023-04-23 10:14:56
  * @LastEditors: YIDA-max 3136271519@qq.com
- * @LastEditTime: 2023-05-01 09:42:26
+ * @LastEditTime: 2023-05-04 14:46:47
  * @FilePath: /node-koa-mysql/src/routes/pixiv/index.ts
  * @Description: 路由入口之一
  */
@@ -10,6 +10,7 @@ import logo from "./logo";
 import refresh from "./refresh";
 import recommend from "./recommend";
 import referer from "./referer";
+import illustration from "./illustration";
 import Router from "koa-router";
 const router = new Router();
 
@@ -23,5 +24,6 @@ router.post("/pixiv/refresh", refresh);
 router.post("/pixiv/recommend", recommend);
 // 获取展示图片的链接
 router.post("/pixiv/referer", referer);
-
+// 获取到对应的id的图片信息的接口
+router.post("/pixiv/illustration", illustration);
 export default router;
