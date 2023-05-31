339a8b3c34d9fb885e73b4ff1a667eac94e66f25
YIDA-max <3136271519@qq.com>
2023-05-31 10:47:05 +0800

    feat: epub阅读功能补充

--- src/routes/fiction/myFictionItemInfo.ts
+++ src/routes/fiction/myFictionItemInfo.ts
@@ -2,7 +2,7 @@
  * @Author: YIDA-max 3136271519@qq.com
  * @Date: 2023-04-24 14:55:54
  * @LastEditors: YIDA-max 3136271519@qq.com
- * @LastEditTime: 2023-05-29 15:22:49
+ * @LastEditTime: 2023-05-30 15:54:14
  * @FilePath: /node-koa-mysql/src/routes/fiction/myFictionItemInfo.ts
  * @Description
  *
@@ -19,12 +19,15 @@ export default async (ctx: RouterContext, next: () => any) => {
   }
   const { success, data: epub } = await getMyFictionItem(name);
   if (success) {
+    const filename = epub.filename.split("/").pop();
     ctx.body = {
       title: epub.metadata.title,
       creator: epub.metadata.creator,
       date: epub.metadata.date,
       toc: epub.toc,
       contents: epub.spine.contents,
+      epub,
+      filename,
     };
   }
 };
