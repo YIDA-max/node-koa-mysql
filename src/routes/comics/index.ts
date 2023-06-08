36ec23db461ba995eadc335cd65efdafbddf5133
YIDA-max <3136271519@qq.com>
2023-06-08 16:54:50 +0800

    feat: 后台漫画柜Table漫画展示查询功能

--- src/routes/comics/index.ts
+++ src/routes/comics/index.ts
@@ -2,12 +2,13 @@
  * @Author: YIDA-max 3136271519@qq.com
  * @Date: 2023-04-23 10:14:56
  * @LastEditors: YIDA-max 3136271519@qq.com
- * @LastEditTime: 2023-06-06 10:45:47
+ * @LastEditTime: 2023-06-08 11:30:30
  * @FilePath: /node-koa-mysql/src/routes/comics/index.ts
  * @Description: 路由入口之一
  */
 import comicsHtml from "./comicsHtml";
 import getComicList from "./getComicList";
+import comicsTable from "./comicsTable";
 import search from "./search";
 import Router from "koa-router";
 const router = new Router();
@@ -18,4 +19,6 @@ router.get("/comics/comicsHtml", comicsHtml);
 router.get("/comics/getComicList", getComicList);
 // 漫画搜索接口
 router.post("/comics/search", search);
+// 漫画推荐搜索列表接口
+router.get("/comics/comicsTable", comicsTable);
 export default router;
