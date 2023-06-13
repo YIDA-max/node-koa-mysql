53941ac46c99c91be8e219073b383ecacc7f8f21
YIDA-max <3136271519@qq.com>
2023-06-14 07:57:24 +0800

    feat(模块): 第一版主下载功能

--- src/routes/01BZ/text.ts
+++ src/routes/01BZ/text.ts
@@ -2,7 +2,7 @@
  * @Author: YIDA-max 3136271519@qq.com
  * @Date: 2023-04-24 14:55:54
  * @LastEditors: YIDA-max 3136271519@qq.com
- * @LastEditTime: 2023-06-09 17:31:50
+ * @LastEditTime: 2023-06-10 08:37:20
  * @FilePath: /node-koa-mysql/src/routes/01BZ/text.ts
  * @Description
  *
@@ -11,13 +11,15 @@
 import { RouterContext } from "koa-router";
 import axios from "axios";
 import { load } from "cheerio";
+import { text } from "./utils/text";
 const puppeteer = require("puppeteer");
 export default async (ctx: RouterContext, next: () => any) => {
   try {
-    const { PHPSESSID, cf_clearance, url } = ctx.request.body as {
+    const { PHPSESSID, cf_clearance, url, name } = ctx.request.body as {
       PHPSESSID: string;
       cf_clearance: string;
       url: string;
+      name: string;
     };
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -42,6 +44,7 @@ export default async (ctx: RouterContext, next: () => any) => {
     });
     const content = await page.content();
     const $ = load(content);
+    ctx.body = await text($, page, url, name);
     ctx.body = content;
   } catch (error) {
     console.log(" ", error);
